adcc7f86895e10146cb3996e6e8f26c440039745
“ijassm” <ijass.ocean@gmail.com>
2024-05-16 11:10:14 +0530

    Completed for loop & nested for loop

---
+++ for-loop.py
@@ -0,0 +1,27 @@
+##range(start, stop, step)
+
+##for example
+##range(5) => range(0, 5, 1)
+##range(1,5) => range(1, 5, 1)
+##range(1,5,2) => range(1, 5, 2)
+##1
+##3
+
+##for i in range(5):
+##    print(i)
+
+##for i in range(1,5):
+##    print(i)
+
+##for i in range(1,5,2):
+##    print(i)
+
+for i in range(1,101):
+    if i % 2 == 0:
+        print("Even -", i)
+    else:
+        print("Odd -", i)
+
+
+
+    
---
+++ nested-for-loop.py
@@ -0,0 +1,41 @@
+##for i in range(2):
+##    for j in range(2):
+##        print(j,i)
+        
+    
+##i = 0
+        ##j = 0 | ##j = 1
+##i = 1
+        ##j = 0 | ##j = 1
+
+"""
+0 0
+1 0
+0 1
+1 1
+"""
+
+
+"""
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+"""
+        
+##for i in range(1,26):
+##    print("*", end=" ")
+##    if i % 5 == 0:
+##        print()
+
+##for i in range(1,6):
+##    print("* " * 5)
+
+for i in range(5):
+    for j in range(5):
+        print("*", end=" ")
+    print()
+
+
+      
