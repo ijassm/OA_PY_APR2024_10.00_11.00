4dbead46f771391ae6f6e23ea0ba5aac4368550d
“ijassm” <ijass.ocean@gmail.com>
2024-05-21 10:40:42 +0530

    Added : Completed control-statement & loop-else

---
+++ control-statements.py
@@ -0,0 +1,16 @@
+# continue
+# break
+# pass
+
+# if True:
+#     pass
+
+# for i in range(1, 101):
+#     if i % 2 == 0:
+#         continue
+#     print(i)
+
+# for i in range(1, 101):
+#     if i == 10:
+#         break
+#     print(i)
---
+++ loop-else.py
@@ -0,0 +1,14 @@
+# for i in range(1, 10):
+#     print(i)
+
+# else:
+#     print("Loop executed successfully")
+
+i = 0
+
+while i < 10:
+    i = i + 1
+    print("Hello World", i)
+
+else:
+    print("Loop executed successfully")
