e5d1874e41ce84f6dc59e1cb3a92573d3f846515
“ijassm” <ijass.ocean@gmail.com>
2024-05-22 11:10:26 +0530

    Added : Started String Manipulation

---
+++ string-manipulation.py
@@ -0,0 +1,81 @@
+a = "Batman"
+b = "Superman"
+
+# iterable datatype
+
+# for i in a:
+#     print(i, end=" ")
+
+# print("✨🧨", end=" ")
+
+# for i in b:
+#     print(i, end=" ")
+
+# Accessing the datatype
+# index
+
+print("a[0]", a[0])
+print("a[1]", a[1])
+print("a[2]", a[2])
+print("a[3]", a[3])
+print("a[4]", a[4])
+print("a[5]", a[5])
+print("🎈")
+print("b[0]", b[0])
+print("b[1]", b[1])
+print("b[2]", b[2])
+print("b[3]", b[3])
+print("b[4]", b[4])
+print("b[5]", b[5])
+print("b[6]", b[6])
+print("b[7]", b[7])
+
+print("Negative Index👓")
+print("Batman✨")
+print("a[-1]", a[-1])
+print("a[-2]", a[-2])
+print("a[-3]", a[-3])
+print("a[-4]", a[-4])
+print("a[-5]", a[-5])
+print("a[-6]", a[-6])
+
+print("Negative Index👓")
+print("Superman🧨")
+print("b[-1]", b[-1])
+print("b[-2]", b[-2])
+print("b[-3]", b[-3])
+print("b[-4]", b[-4])
+print("b[-5]", b[-5])
+print("b[-6]", b[-6])
+print("b[-7]", b[-7])
+print("b[-8]", b[-8])
+
+print("Accessing through index in string💫")
+
+for i in range(len(a)):
+    print(a[i])
+
+print("🎈")
+
+for i in range(len(b)):
+    print(b[i])
+
+# Strings are immutable
+
+# a[0] = "S"
+# b[0] = "B"
+
+
+# String Slicing
+# variable[start:stop:step]
+# variable[1] => variable[0:1:1]
+# variable[1:2] => variable[1:2:1]
+# variable[1:4:2] => variable[1:4:2]
+# variable[1::2] => variable[1:end:2]
+# variable[::2] => variable[start:end:2]
+
+print("a[0:5]", a[0:5])
+print("a[0:6]", a[0:6])
+print("a[0:7]", a[0:7])
+print("a[0:8]", a[0:8])
+print("a[0:9]", a[0:9])
