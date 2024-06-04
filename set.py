67682302b2d40b44541c17fcf13291dff5816e06
“ijassm” <ijass.ocean@gmail.com>
2024-06-04 10:49:55 +0530

    Added : Completed set and tuple

---
+++ set.py
@@ -0,0 +1,13 @@
+s = {2434, 5, 6, 7, True, 1, 2, 0, False}
+
+print(s)
+print(type(s))
+
+# it won't allow duplicates
+# unordered
+
+s.add(10)
+print(s)
+
+s.remove(10)
+print(s)
---
+++ tuple.py
@@ -0,0 +1,39 @@
+t = 1, 3, 45, 23, 3
+
+print(type(t))
+print(t)
+
+# positive index
+print(f"t[0] - {t[0]}")
+print(f"t[1] - {t[1]}")
+print(f"t[2] - {t[2]}")
+print(f"t[3] - {t[3]}")
+
+# negative index
+
+print(f"t[-1] - {t[-1]}")
+print(f"t[-2] - {t[-2]}")
+print(f"t[-3] - {t[-3]}")
+print(f"t[-4] - {t[-4]}")
+
+# immutable
+
+# t[0] = 5
+
+# iterable
+
+# l = []
+
+# for i in t:
+#     l.append(i)
+
+# print(l)
+
+# methods
+
+# print(t.count(3))
+# print(t.index(3))
+
+# slicing
+
+print(t[1:3])
