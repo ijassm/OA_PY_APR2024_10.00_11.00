7fab77954408cee3e212d7a557e09e9270290e9e
“ijassm” <ijass.ocean@gmail.com>
2024-06-11 13:20:14 +0530

    Added : started dict

---
+++ dict.py
@@ -0,0 +1,19 @@
+l = ["Tokiyo", 25, "London"]
+
+person = {
+    "name": "Tokiyo",
+    "age": 25,
+    "city": "London",
+}
+
+# print(type(l))
+# print(type(person))
+
+person["age"] = person["age"] + 1
+
+person["skills"] = ["PYTHON", "JAVA", "C", "C++"]
+
+print(person["name"])
+print(person["age"])
+print(person["city"])
+print(person["skills"])
