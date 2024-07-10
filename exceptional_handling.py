9b9124186f1161a76ca751bbd9f6f3d9fd28eccf
“ijassm” <ijass.ocean@gmail.com>
2024-07-10 10:45:30 +0530

    Patched : execeptional handling for thilaga

--- exceptional_handling.py
+++ exceptional_handling.py
@@ -1,23 +1,38 @@
-# Exceptional handling
-sakthi = "Good Boy"
+# # Exceptional handling
+# sakthi = "Good Boy"
 
-try:
-    print("First Try")
-    print(5 / 2)
-    print(sakthi)
-    print(5 + "5")
+# try:
+#     print("First Try")
+#     print(5 / 2)
+#     print(sakthi)
+#     print(5 + "5")
 
-except ZeroDivisionError:
-    print("Can't divide by zero")
+# except ZeroDivisionError:
+#     print("Can't divide by zero")
 
-except NameError:
-    print("Please check your varibale is decalared or not")
+# except NameError:
+#     print("Please check your varibale is decalared or not")
 
-except Exception as error:
-    print("Something went wrong : ", error)
+# except Exception as error:
+#     print("Something went wrong : ", error)
 
-else:
-    print("No exception occurred")
+# else:
+#     print("No exception occurred")
 
-finally:
-    print("Finally block executed")
+# finally:
+#     print("Finally block executed")
+
+# name = "thilaga"
+
+# try:
+#     print(5 + "5")
+#     print(name)
+#     print(4 / 2)
+# except ZeroDivisionError:
+#     print("Can't divide by zero")
+# except Exception as error:
+#     print("Something went wrong : ", error)
+# else:
+#     print("program executed successfully without any errors")
+# finally:
+#     print("finally whatever error happened it will execute")
