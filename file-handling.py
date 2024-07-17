cdbbe8952d0d82de5a930569f1b8895ccab511f6
“ijassm” <ijass.ocean@gmail.com>
2024-07-17 11:16:59 +0530

    completed file handling

---
+++ file-handling.py
@@ -0,0 +1,22 @@
+# import os
+
+# for i in range(1, 6):
+#     os.remove(f"test{i}.py")
+
+# file = open("test1.py", "rt")
+
+# print(file.read())
+
+# file.close()
+
+# file = open("test1.py", "at")
+
+# print(file.write("b = a"))
+
+# file.close()
+
+# file = open("test1.py", "wt")
+
+# print(file.write("a = 55"))
+
+# file.close()
